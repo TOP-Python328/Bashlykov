31eab23102113f634f5fa7ca3d62d34eac625212
bashlykov2005 <bashlykov2005@mail.ru>
2023-06-04 22:19:36 +0500

    задания 5 и 6

---
+++ 2023.05.28/5.py
@@ -0,0 +1,19 @@
+square_1 = input('')
+square_2 = input('')
+
+if square_1[0] == square_2[0] or square_1[1] == square_2[1]:
+    print('да')
+else:
+    print('нет')
+    
+# a1
+# a7
+# да
+
+# a1
+# f1
+# да
+
+# d1
+# c4
+# нет
---
+++ 2023.05.28/6.py
@@ -0,0 +1,23 @@
+square_1 = input('')
+square_2 = input('')
+
+if square_1[0] == square_2[0] and -1 <= int(square_1[1]) - int(square_2[1]) <= 1:
+    print('да')
+elif square_1[1] == square_2[1] and -1 <= ord(square_1[0]) - ord(square_2[0]) <= 1:
+    print('да')
+elif -1 <= ord(square_1[0]) - ord(square_2[0]) <= 1 and -1 <= int(square_1[1]) - int(square_2[1]) <= 1:
+    print('да')
+else:
+    print('нет')
+    
+# a4
+# a5
+# да
+
+# d3
+# e4
+# да
+
+# f6
+# a1
+# нет
