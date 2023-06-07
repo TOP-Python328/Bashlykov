2b42ba2a704332f1ec3f3dcd9bcad3432a817355
Gennadiy <renardes@gmail.com>
2023-06-07 16:40:59 +0500

    2023.05.28: комментарии, правки, итоги

--- 2023.05.28/6.py
+++ 2023.05.28/6.py
@@ -1,10 +1,13 @@
 square_1 = input('')
 square_2 = input('')
 
+# УДАЛИТЬ: условия избыточны
 if square_1[0] == square_2[0] and -1 <= int(square_1[1]) - int(square_2[1]) <= 1:
     print('да')
+# УДАЛИТЬ: условия избыточны
 elif square_1[1] == square_2[1] and -1 <= ord(square_1[0]) - ord(square_2[0]) <= 1:
     print('да')
+# КОММЕНТАРИЙ: вот этих проверок достаточно
 elif -1 <= ord(square_1[0]) - ord(square_2[0]) <= 1 and -1 <= int(square_1[1]) - int(square_2[1]) <= 1:
     print('да')
 else:
@@ -24,3 +27,4 @@ else:
 # нет
 
 
+# ИТОГ: хорошо — 3/4
