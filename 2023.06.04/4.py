76f9e3ca053917373011984dee994dc9c16f274e
Alexander <127608153+bashlykov2005@users.noreply.github.com>
2023-06-12 22:29:09 +0500

    Update 4.py

--- 2023.06.04/4.py
+++ 2023.06.04/4.py
@@ -1,18 +1,17 @@
-# num_position = int(input('Введите количество разрядов: '))
-# max_num = int(num_position * '9')
-# count_numbers = 0
+num_digit = int(input('Введите количество разрядов: '))
+max_num = int(num_digit * '9')
+count_numbers = 0
 
-# for num in range((max_num + 1) // 10, max_num + 1):
-    # div = 2
-    # while div * div <= num:
-        # if not num % div:
-            # break
-        # div += 1
-    # else: 
-        # count_numbers += 1
-# print(count_numbers)        
+for num in range((max_num + 1) // 10, max_num + 1):
+    for x in range(2, num):
+        if num % x == 0:
+            break
+    else:
+        count_numbers += 1
+print(count_numbers)
+    
+# Введите количество разрядов: 3
+# 143
 
-text = input()
-punctuation = '.,:;!?–—\'\"()*/'
-text_clean = ''.join(char for char in text if char not in punctuation)
-print(text_clean)
+# Введите количество разрядов: 5
+# 8363
