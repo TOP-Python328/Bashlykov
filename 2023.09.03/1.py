ee2ef4490dd7bc3b41b1c6db26b3c6771597a2d7
Gennadiy <renardes@gmail.com>
2023-09-16 10:30:39 +0500

    2023.09.03: комментарии, правки, итоги

--- 2023.09.03/1.py
+++ 2023.09.03/1.py
@@ -2,7 +2,8 @@ class Tetrahedron:
     """Класс с методами вычисления площади поверхности и объема тела тетраэдра"""
     def __init__(self, edge: float):
         self.edge = float(edge)
-       
+
+    # УДАЛИТЬ: не было в задании такого метода
     def edge(self) -> float:
         return self.edge
     
@@ -13,9 +14,8 @@ class Tetrahedron:
     def volume(self) -> float:
         """Вычисляет объём тела"""
         return self.edge**3 * 2**0.5 / 12
-        
-# C:\Users\Asus\OneDrive\Рабочий стол\Program\dz_python\Bashlykov\2023.09.03
- # 14:46:49 > python -i 1.py
+
+
 # >>> t1 = Tetrahedron(5)
 # >>> t1.edge
 # 5.0
@@ -30,4 +30,6 @@ class Tetrahedron:
 # 62.35382907247958
 # >>> t2.volume()
 # 25.455844122715714
-# >>>
+
+
+# ИТОГ: очень хорошо — 3/3
--- 2023.09.03/2.py
+++ 2023.09.03/2.py
@@ -1,45 +1,49 @@
-from decimal import Decimal as  dcm
+# ИСПОЛЬЗОВАТЬ: общепринятым сокращением Decimal является dec
+from decimal import Decimal as dec
 from datetime import time, date, datetime
-import numbers
+# ИСПОЛЬЗОВАТЬ: в задании я пишу квалифицированное имя класса, включающее модуль, чтобы вы не запутались в библиотеках — к импорту это никак не относится, из стандартной библиотеки по-прежнему берём всё с помощью from ... import ...
+from numbers import Number
+
 
 class PowerMeter:
     """Описывает двухтарифный счётчик потреблённой электрической мощности"""
-    def __init__(self,
-            tariff1: numbers.Number = 2.89,
-            tariff2: numbers.Number = 2.01,
+    def __init__(
+            self,
+            tariff1: Number = 2.89,
+            tariff2: Number = 2.01,
+            # ИСПРАВИТЬ: а я ведь говорил: не запутайтесь между модулем и классом — вот вы и запутались, сейчас идентификатор datetime ссылается на объект класса, а не модуля, и только специфическая структура конкретно этих классов уберегла вас от исключения
             tariff2_starts: datetime.time = time(23,00),
-            tariff2_ends: datetime.time = time(7,00)): 
-        self.tariff1 = dcm(tariff1)
-        self.tariff2 = dcm(tariff2)
+            tariff2_ends: datetime.time = time(7,00)
+    ):
+        self.tariff1 = dec(tariff1)
+        self.tariff2 = dec(tariff2)
         self.tariff2_starts = tariff2_starts
         self.tariff2_ends = tariff2_ends
-        self.power: dcm = dcm(0)
+        self.power: dec = dec(0)
+        # ИСПРАВИТЬ: во-первых, этот атрибут нигде не обновляется, а значит речь идёт не о "текущей дате", а о "дате создания"; во-вторых, такой атрибут скорее мешает, чем помогает (см. комментарий к __str__())
         self.current_date: datetime.date = date.today().strftime("01.%m.%Y")
-        self.charges: dict[datetime.date, dcm] = {self.current_date: self.power}
-        
-    
-    def __repr__(self):
-        return f'<{self.__class__.__name__} : {self.power} кВт/ч>'
-        
+        self.charges: dict[datetime.date, dec] = {self.current_date: self.power}
 
-    def __str__(self):    
-        return f'({date.today().strftime("%B")}) {self.charges[self.current_date]}'
-        
-        
-    def meter(self, power: numbers.Number) -> dcm:
+    def __repr__(self):
+        return f'<{self.__class__.__name__}: {self.power} кВт/ч>'
+
+    def __str__(self):
+        # ИСПРАВИТЬ: если я через месяц после создания экземпляра запрошу строковое представление данного счётчика, то увижу данные за всё тот же первый месяц — это вряд ли соответствует замыслу
+        return f'({date.today().strftime("%b")}) {self.charges[self.current_date]}'
+
+    def meter(self, power: Number) -> dec:
         """Вычисляет и возвращает стоимость потреблённой мощности, соответствующей переданному значению потреблённой мощности"""
-        power = dcm(power)
+        power = dec(power)
         if self.tariff2_ends < datetime.now().time() < self.tariff2_starts:
             power = self.tariff1 * power
         else:
             power = self.tariff2 * power
-        self.power += power.quantize(dcm("1.00"))
-        self.charges[self.current_date] += power.quantize(dcm("1.00"))
+        self.power += power.quantize(dec("1.00"))
+        self.charges[self.current_date] += power.quantize(dec("1.00"))
         
-        return power.quantize(dcm("1.00"))
-        
-# C:\Users\Asus\OneDrive\Рабочий стол\Program\dz_python\Bashlykov\2023.09.03
- # 21:58:06 > python -i 2.py
+        return power.quantize(dec("1.00"))
+
+
 # >>> pm1 = PowerMeter()
 # >>> pm1.meter(2)
 # Decimal('5.78')
@@ -54,3 +58,6 @@ class PowerMeter:
 # {'01.09.2023': Decimal('9.25')}
 # >>> pm1.power
 # Decimal('9.25')
+
+
+# ИТОГ: хорошо, доработайте немного — 4/6
